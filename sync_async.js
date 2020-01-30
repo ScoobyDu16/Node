b68398538de25d6452676d58d11cadc8b6a46eb0
AmarSandhu <sandhuamar07@gmail.com>
2020-01-30 09:55:57 +0530

    Hey There

---
+++ sync_async.js
@@ -0,0 +1,18 @@
+//UnComment the Code to execute
+
+//****Sync****
+// var fs = require('fs');
+// var data = fs.readFileSync('text.txt');
+// console.log(data.toString());
+// console.log('End Here');
+
+//****Async****
+// var fs = require('fs');
+// fs.readFile('text.txt', function(err, data){
+//     if(err){
+//         console.log(err);
+//     } else{
+//         console.log(data.toString());
+//     }
+// });
+// console.log('Start Here');
